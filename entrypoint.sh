513b3509435dc93b81a0e7ebf460052ea95a9484
Mike Cooper <mythmon@gmail.com>
2019-01-21 11:49:01 -0800

    Debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,6 @@ set -e
 GIT_AUTHOR_NAME=${GIT_AUTHOR_NAME-"Automated Github Action"}
 GIT_AUTHOR_EMAIL=${GIT_AUTHOR_EMAIL-"actions@github.io"}
 
-git config --global user.name "$GIT_AUTHOR_NAME"
-git config --global user.email "$GIT_AUTHOR_EMAIL"
-
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
@@ -17,5 +14,12 @@ if [ -n "$NPM_AUTH_TOKEN" ]; then
   printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_AUTH_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
+
+set -x
+
+git config --global user.name "$GIT_AUTHOR_NAME"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
+
+get config --list
 
 sh -c "npm $*"
