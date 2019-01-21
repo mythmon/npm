fb6b77bf2154131f58a5dd65b67cc5c6ad50e2f6
Mike Cooper <mythmon@gmail.com>
2019-01-21 11:38:03 -0800

    Explicitly set git authorship

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-export GIT_AUTHOR_NAME=${GIT_AUTHOR_NAME-"Automated Github Action"}
-export GIT_AUTHOR_EMAIL=${GIT_AUTHOR_EMAIL-"actions@github.io"}
+GIT_AUTHOR_NAME=${GIT_AUTHOR_NAME-"Automated Github Action"}
+GIT_AUTHOR_EMAIL=${GIT_AUTHOR_EMAIL-"actions@github.io"}
+
+git config user.name "$GIT_AUTHOR_NAME"
+git config user.email "$GIT_AUTHOR_EMAIL"
 
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
